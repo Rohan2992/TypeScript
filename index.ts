d02e10ebe6d9a953e891af2128351279c4153091
Rohan2992 <rohan.chaudhary29.rc@gmail.com>
2023-10-08 22:47:39 +0530

    calculator

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
-function sumOfNumbers(a: number, b: number): number {
-  return a + b;
-}
-
-const addition = sumOfNumbers(5, 4);
-console.log(addition);
-
-function calculate(a: number, b: number, type: string): number | undefined {
+function calculate(
+  a: number,
+  b: number,
+  type: "add" | "sub" | "div" | "mul"
+): number {
   if (type === "add") return a + b;
   else if (type === "sub") return a - b;
   else if (type === "mul") return a * b;
@@ -13,5 +10,11 @@ function calculate(a: number, b: number, type: string): number | undefined {
   return -1;
 }
 
-const cal = calculate(5, 4, "");
+let cal: number = calculate(5, 4, "");
+console.log(cal);
+cal = calculate(5, 4, "sub");
+console.log(cal);
+cal = calculate(5, 4, "mul");
+console.log(cal);
+cal = calculate(5, 4, "div");
 console.log(cal);
