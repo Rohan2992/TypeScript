779f8ffa30c2835f7dcf1e184871c8dd2af65af7
Rohan2992 <rohan.chaudhary29.rc@gmail.com>
2023-10-09 12:07:46 +0530

    implementing interfaces

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 "use strict";
-function sumOfNumbers(a, b) {
-    return a + b;
+function greet(person) {
+    console.log(person, features);
+    return ("Hello, " + person.name + " glad to hear you r " + person.age + " years old.");
 }
-const addition = sumOfNumbers(5, 4);
-console.log(addition);
-function calculate(a, b, type) {
-    if (type === "add")
-        return a + b;
-    else if (type === "sub")
-        return a - b;
-    else if (type === "mul")
-        return a * b;
-    else if (type === "div")
-        return a / b;
-    // return -1;
-}
-const cal = calculate(5, 4, "add");
-console.log(cal);
+let greetMsg = greet({ name: "Rohan", age: 21, gender: "male", hairColor: "black", color: "white", nationality: "Indian" });
+console.log(greetMsg);
--- index.ts
+++ index.ts
@@ -1,20 +1,34 @@
-function calculate(
-  a: number,
-  b: number,
-  type: "add" | "sub" | "div" | "mul"
-): number {
-  if (type === "add") return a + b;
-  else if (type === "sub") return a - b;
-  else if (type === "mul") return a * b;
-  else if (type === "div") return a / b;
-  return -1;
+interface Person {
+  name: string;
+  gender: string;
+  age: number;
+  features: characteristics
 }
 
-let cal: number = calculate(5, 4, "");
-console.log(cal);
-cal = calculate(5, 4, "sub");
-console.log(cal);
-cal = calculate(5, 4, "mul");
-console.log(cal);
-cal = calculate(5, 4, "div");
-console.log(cal);
+interface characteristics {
+  hairColor: string;
+  color: string;
+  nationality: string;
+}
+
+class livingBeings implements Person {
+  name: string;
+  gender: string;
+  age: number;
+  features: characteristics;
+
+  constructor(name: string, gender: string, age: number, hairColor: string, color: string, nationality: string) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.features = { hairColor: hairColor, color: color, nationality: nationality };
+  }
+  greet() {
+    console.log(this.features);
+    return ("Hello, " + this.name + " glad to hear you r " + this.age + " years old.");
+  }
+}
+
+const personObject = new livingBeings("Rohan", "male", 21, "black", "Indian", "brown");
+const op = personObject.greet();
+console.log(op);
