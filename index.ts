a6ba5debe3f754ed32b5fb49acf5992aab883b4f
Rohan2992 <rohan.chaudhary29.rc@gmail.com>
2023-10-11 19:53:41 +0530

    generics and literals

--- index.ts
+++ index.ts
@@ -1,30 +1,13 @@
-type Arithmetic = "add" | "sub" | "mul" | "div";
-enum Arithmetic1 { add = 1, sub, mul, div }
+// how to pass array as argument
 
-function calculate(a: number, b: number, type: number): number {
-  if (type === 1) return a + b;
-  else if (type === 2) return a - b;
-  else if (type === 3) return a * b;
-  else if (type === 4) return a / b;
 
-  return -1;
+function firstElement<T>(arr: T[]): T {
+  return arr[0];
 }
 
-const ans = calculate(10, 50, Arithmetic1.add);
-console.log(ans);
+const ans = firstElement<number>([1, 2]);
+const ans1 = firstElement<string>(["1", "2"]);
 
-type Arithmetic2 = {
-  value: string;
-  operator: string;
-};
+// ans1.tolowerCase();
 
-interface A2 extends Arithmetic2 {
-  vs: string
-}
-
-
-function greet(value: A2) {
-  return value
-}
-
-console.log(greet({ value: 'hello', operator: "+", vs: "code" }));
+console.log(ans, ans1)
--- readme.md
+++ readme.md
@@ -19,3 +19,28 @@
 - type allows to do unions and ors
 - Types cannot be extended
 - A interface can extend a type whereas vice-versa is not allowed.
+
+# Type inference - it is the mechanism by which ts automatically infer the return type of a function and variables
+
+- try making the variable as number or string in ts
+
+# Backend validation
+
+- can be done sending the wrong inputs or exposing the folders to the end-user
+
+  - system("ls") - c++ code #include<cstdlib> - to import system library
+
+- frontend - npm run , node dis , npm run start
+- backend - tsc node dist/index.js
+
+- Input Validation
+
+TRPC - used to add backend validations and send them to frontend
+
+# Process Managers
+
+- these runs the backend automatically when it goes down
+- modules - forever & pm2
+- pm2 start file_name // pm2 logs // pm2 list // pm2 kill
+
+# using the zod library
